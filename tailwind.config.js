bec95135f0522edf7068685622621b3b96fb33a3
Ivan <ivan.y.badaev@gmail.com>
2024-07-30 16:48:54 +0300

    renamed & reconfigured Project to Ditnails

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/pages/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'black' : 'rgba(26, 26, 26, 1)', 
+        'lightgray' : 'rgba(234, 234, 235, 1)',
+        'lightgrey' : 'rgba(234, 234, 235, 1)',
+        'gray' : 'rgba(214, 214, 214, 1)',
+        'grey' : 'rgba(214, 214, 214, 1)',
+        'darkgray' : 'rgba(90, 90, 90, 1)',
+        'darkgrey' : 'rgba(90, 90, 90, 1)',
+      },
+      fontSize : {
+        '80' : '80px',
+        '23' : '23px',
+        '14' : '14px',
+      }
+    },
   },
   plugins: [],
 }
